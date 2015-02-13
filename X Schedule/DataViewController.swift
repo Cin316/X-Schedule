767877b98101b78da9916016ab0d16a5d9cdf391
Nicholas Reichert <joe13reich@gmail.com>
2015-02-13 15:37:44 -0500

    Set up in-app data view for testing.

---
+++ X Schedule/DataViewController.swift
@@ -0,0 +1,25 @@
+//
+//  DataViewController.swift
+//  X Schedule
+//
+//  Created by Nicholas Reichert on 2/13/15.
+//  Copyright (c) 2015 Nicholas Reichert. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
