2ce2aeef907ef6490fa601249e256f2308e831fd
Nicholas Reichert <joe13reich@gmail.com>
2015-02-16 16:24:16 -0500

    Added Label to GUI to visualize output for debugging. Added outlet for labelBox to DataViewController.

--- X Schedule/DataViewController.swift
+++ X Schedule/DataViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class DataViewController: UIViewController {
     
+    @IBOutlet weak var displayBox: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
