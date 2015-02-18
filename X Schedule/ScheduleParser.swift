e488296abd437e6bf2ca2cfc1efea91841449dde
Nicholas Reichert <joe13reich@gmail.com>
2015-02-18 13:04:55 -0500

    Added ScheduleParser class with empty method declaration for parseForSchedule.

---
+++ X Schedule/ScheduleParser.swift
@@ -0,0 +1,24 @@
+//
+//  ScheduleParser.swift
+//  X Schedule
+//
+//  Created by Nicholas Reichert on 2/18/15.
+//  Copyright (c) 2015 Nicholas Reichert. All rights reserved.
+//
+
+import Foundation
+
+class ScheduleParser {
+
+    class func parseForSchedule(string: String) -> Schedule {
+        //Setup variables
+        var items = [ScheduleItem]()
+        var schedule = Schedule(items: items)
+        
+        //Put parsing code here.
+        
+        //Return finished schedule.
+        return schedule
+    }
+
+}
