31b798a35093b341b06e09bbce9bbad21be1aed1
Nicholas Reichert <joe13reich@gmail.com>
2015-02-25 22:18:46 -0500

    Added title and date labels into GUI and implemented functionality.

--- X Schedule/DataViewController.swift
+++ X Schedule/DataViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class DataViewController: UIViewController {
     
+    @IBOutlet weak var scheduleDate: UILabel!
+    @IBOutlet weak var scheduleTitle: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,12 +23,21 @@ class DataViewController: UIViewController {
                 //Execute code in main thread.
                 dispatch_async(dispatch_get_main_queue()) {
                     var parser = ScheduleParser()
+                    //Parse the downloaded code for schedule.
                     var schedule = parser.parseForSchedule(output)
+                    //Display schedule items in table.
                     if let tableController = self.childViewControllers[0] as? ScheduleTableController {
                         tableController.schedule = schedule
                         let tableView = (tableController.view as? UITableView)!
                         tableView.reloadData()
                     }
+                    //Display title.
+                    self.scheduleTitle.text = schedule.title
+                    //Display correctly formatted date.
+                    var dateFormatter = NSDateFormatter()
+                    dateFormatter.dateFormat = "EEEE, MMMM d"
+                    self.scheduleDate.text = dateFormatter.stringFromDate(schedule.date)
+
                 }
             }
         )
