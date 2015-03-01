25b48e25c90bfa8a059bf9c404346f85ec818204
Nicholas Reichert <joe13reich@gmail.com>
2015-02-28 19:57:07 -0500

    Removed "All rights reserved." from all files.

--- X Schedule/ScheduleParser.swift
+++ X Schedule/ScheduleParser.swift
@@ -3,7 +3,7 @@
 //  X Schedule
 //
 //  Created by Nicholas Reichert on 2/18/15.
-//  Copyright (c) 2015 Nicholas Reichert. All rights reserved.
+//  Copyright (c) 2015 Nicholas Reichert.
 //
 
 import Foundation
--- X Schedule/ScheduleTableController.swift
+++ X Schedule/ScheduleTableController.swift
@@ -3,7 +3,7 @@
 //  X Schedule
 //
 //  Created by Nicholas Reichert on 2/21/15.
-//  Copyright (c) 2015 Nicholas Reichert. All rights reserved.
+//  Copyright (c) 2015 Nicholas Reichert.
 //
 
 import UIKit
