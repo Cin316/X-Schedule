f27bbe649c63aace3b7e2b01c1456f15686e0fa8
Nicholas Reichert <joe13reich@gmail.com>
2015-04-08 22:49:22 -0400

    Added empty XScheduleKit swift framework.

---
+++ XScheduleKit/XScheduleKit.h
@@ -0,0 +1,19 @@
+//
+//  XScheduleKit.h
+//  XScheduleKit
+//
+//  Created by Nicholas Reichert on 4/8/15.
+//  Copyright (c) 2015 Nicholas Reichert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XScheduleKit.
+FOUNDATION_EXPORT double XScheduleKitVersionNumber;
+
+//! Project version string for XScheduleKit.
+FOUNDATION_EXPORT const unsigned char XScheduleKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XScheduleKit/PublicHeader.h>
+
+
