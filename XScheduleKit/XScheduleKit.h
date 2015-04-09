7540f313fdb2c0d462eaaf4411fe241476424db8
Nicholas Reichert <joe13reich@gmail.com>
2015-04-09 17:48:32 -0400

    Removed "All rights reserved" notice from XScheduleKit.h.

--- XScheduleKit/XScheduleKit.h
+++ XScheduleKit/XScheduleKit.h
@@ -3,7 +3,7 @@
 //  XScheduleKit
 //
 //  Created by Nicholas Reichert on 4/8/15.
-//  Copyright (c) 2015 Nicholas Reichert. All rights reserved.
+//  Copyright (c) 2015 Nicholas Reichert.
 //
 
 #import <UIKit/UIKit.h>
