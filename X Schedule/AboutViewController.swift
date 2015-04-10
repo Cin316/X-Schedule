6267b39726009e8be1a93aa36157e7c1c7005685
Nicholas Reichert <joe13reich@gmail.com>
2015-04-09 22:32:08 -0400

    Fixed background color on About View on iPad being white instead of light blue.

--- X Schedule/AboutViewController.swift
+++ X Schedule/AboutViewController.swift
@@ -39,4 +39,8 @@ class AboutViewController: UITableViewController, UITableViewDelegate {
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
     }
     
+    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        //Fix background color on iPad.
+        cell.backgroundColor = UIColor(red: (225.0/255.0), green: (238.0/255.0), blue: (254.0/255.0), alpha: 1.0)
+    }
 }
