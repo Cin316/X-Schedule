5931ac5cc964c87fc1224f1a02e9d8b4ca268892
Nicholas Reichert <joe13reich@gmail.com>
2015-04-09 22:28:17 -0400

    Fixed background color of tables on iPad being white instead of light blue.

--- XScheduleKit/ScheduleTableController.swift
+++ XScheduleKit/ScheduleTableController.swift
@@ -50,4 +50,8 @@ public class ScheduleTableController: UITableViewController {
         return cell
     }
     
+    public override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        //Fix background color on iPad.
+        cell.backgroundColor = UIColor(red: (225.0/255.0), green: (238.0/255.0), blue: (254.0/255.0), alpha: 1.0)
+    }
 }
