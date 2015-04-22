da90446353d3f2e2732cc377d09d7814951ef8fe
Nicholas Reichert <joe13reich@gmail.com>
2015-04-21 20:11:16 -0400

    Added color highlighting of the current class to widget. Refractored ScheduleTableControllers so the colors can be changed.

--- XScheduleKit/ColoredScheduleTableController.swift
+++ XScheduleKit/ColoredScheduleTableController.swift
@@ -12,28 +12,14 @@ import UIKit
 
 public class ColoredScheduleTableController: ScheduleTableController {
     
-    public override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
-        
-        //If class is happening right now, make it yellow.
-        let item = schedule.items[indexPath.row]
-        var isNow: Bool = false
-        //If schedule date is today.
-        if (NSCalendar.currentCalendar().isDateInToday(schedule.date)) {
-            //Check if class is happening now.
-            if ((NSDate().compare(item.startTime) != NSComparisonResult.OrderedAscending) && (NSDate().compare(item.endTime) != NSComparisonResult.OrderedDescending)) {
-                //Set background color to yellow
-                isNow = true
-            }
-        }
-        
-        if (isNow) {
-            //Color is yellow.
-            cell.backgroundColor = UIColor(red: (251.0/255.0), green: (250.0/255.0), blue: (146.0/255.0), alpha: 1.0)
-        } else {
-            //Fix background color on iPad.
-            //Color is blue.
-            cell.backgroundColor = UIColor(red: (225.0/255.0), green: (238.0/255.0), blue: (254.0/255.0), alpha: 1.0)
-        }
-        
+    private var internalCellColor: UIColor = UIColor(red: (225.0/255.0), green: (238.0/255.0), blue: (254.0/255.0), alpha: 1.0)
+    private var internalHighlightedColor: UIColor = UIColor(red: (251.0/255.0), green: (250.0/255.0), blue: (146.0/255.0), alpha: 1.0)
+    
+    public override func cellColor() -> UIColor {
+        return internalCellColor
     }
+    public override func highlightedColor() -> UIColor {
+        return  internalHighlightedColor
+    }
+    
 }
