9e19087c4e48f33d8152c604ea1075b296f45a3a
Nicholas Reichert <joe13reich@gmail.com>
2015-04-26 20:34:38 -0400

    Refactored and cleaned up code in ScheduleDownloader. Split downloadSchedule() into multiple functions.

--- XScheduleKit/ScheduleDownloader.swift
+++ XScheduleKit/ScheduleDownloader.swift
@@ -10,29 +10,19 @@ import Foundation
 
 public class ScheduleDownloader {
     
+    private static var scheduleCalenderID: String = "27"
+    private static var scheduleURL: NSURL = NSURL(string: "http://www.stxavier.org/cf_calendar/export.cfm")!
+    
     public class func downloadSchedule(date: NSDate, completionHandler: String -> Void, errorHandler: String -> Void) -> NSURLSessionTask {
-        // Download today's schedule from the St. X website.
-        // Setup for request.
-        var url = NSURL(string:"http://www.stxavier.org/cf_calendar/export.cfm")!
-        var config = NSURLSessionConfiguration.defaultSessionConfiguration()
-        var session = NSURLSession(configuration: config)
+        //Download today's schedule from the St. X website.
+    
+        //Create objects for network request.
+        var postData: NSData = requestDataForDate(date)
+        var request: NSURLRequest = scheduleWebRequest()
+        var session: NSURLSession = scheduleSession()
         
-        var request = NSMutableURLRequest(URL: url)
-        request.HTTPMethod = "POST"
-        request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-        
-        //Get today's date and format it correctly for request.
-        var downloadDate = date
-        var dateFormat = NSDateFormatter()
-        dateFormat.dateFormat = "MM/dd/yyyy"
-        let formattedDate = dateFormat.stringFromDate(downloadDate)
-        let escapedDate = formattedDate.stringByAddingPercentEncodingWithAllowedCharacters(.URLHostAllowedCharacterSet())!
-        
-        //Create NSData object to send in body of POST request.
-        var postData = "export_format=xml&start=\(escapedDate)&end=\(escapedDate)&calendarId=27".dataUsingEncoding(NSUTF8StringEncoding)
-        
-        //Create NSURLSessionUploadTask out of desired settings.
-        var postSession = session.uploadTaskWithRequest(request, fromData: postData, completionHandler:
+        //Create NSURLSessionUploadTask out of desired objects.
+        var postSession: NSURLSessionTask = session.uploadTaskWithRequest(request, fromData: postData, completionHandler:
             { ( data: NSData!, response: NSURLResponse!, error: NSError!) -> Void in
                 //Convert output to a string.
                 var output = NSString(data: data, encoding: NSUTF8StringEncoding) as! String
@@ -49,6 +39,39 @@ public class ScheduleDownloader {
         postSession.resume()
         
         return postSession
+    }
+    private class func scheduleWebRequest() -> NSURLRequest {
+        //Create a new NSURLRequest with the correct parameters to download schedule from St. X Website.
+        var request = NSMutableURLRequest(URL: scheduleURL)
+        request.HTTPMethod = "POST"
+        request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        
+        return request
+    }
+    private class func scheduleSession() -> NSURLSession {
+        //Create a new NSURLSession with default settings.
+        var config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        var session = NSURLSession(configuration: config)
+        
+        return session
+    }
+    private class func requestDataForDate(date: NSDate) -> NSData {
+        //Create NSData object to send in body of POST request.
+        let escapedDate = uploadStringForDate(date)
+        var postString: NSString = "export_format=xml&start=\(escapedDate)&end=\(escapedDate)&calendarId=\(scheduleCalenderID)"
+        var postData: NSData = postString.dataUsingEncoding(NSUTF8StringEncoding)!
+        
+        return postData
+    }
+    private class func uploadStringForDate(date: NSDate) -> String {
+        //Returns a correctly formatted string to upload in as a POST parameter.
+        var dateFormat: NSDateFormatter = NSDateFormatter()
+        dateFormat.dateFormat = "MM/dd/yyyy"
+        dateFormat.locale = NSLocale(localeIdentifier: "en_US_POSIX")
+        let formattedDate: String = dateFormat.stringFromDate(date)
+        let escapedDate: String = formattedDate.stringByAddingPercentEncodingWithAllowedCharacters(.URLHostAllowedCharacterSet())!
+        
+        return escapedDate
     }
     
     public class func downloadSchedule(completionHandler: String -> Void, errorHandler: String -> Void) -> NSURLSessionTask {
