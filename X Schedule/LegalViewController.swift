4af13acb9d67477c2dcd42558ec09c49522256fb
Nicholas Reichert <joe13reich@gmail.com>
2015-04-30 15:12:53 -0400

    Refactored and cleaned up LegalViewController.swift.

--- X Schedule/LegalViewController.swift
+++ X Schedule/LegalViewController.swift
@@ -12,21 +12,21 @@ class LegalViewController: UIViewController {
 
     @IBOutlet weak var mainTextView: UITextView!
     
+    private var licensePath = NSBundle.mainBundle().pathForResource("LICENSE", ofType: "txt")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        var path = NSBundle.mainBundle().pathForResource("LICENSE", ofType: "txt")
-        var licenseText = String(contentsOfFile: path!, encoding: NSUTF8StringEncoding, error: nil)
         
-        if let license = licenseText {
-            mainTextView.text = license
+        loadLegalText()
+    }
+    func loadLegalText() {
+        if let path = licensePath {
+            var licenseText = String(contentsOfFile: path, encoding: NSUTF8StringEncoding, error: nil)
+            if let license = licenseText {
+                mainTextView.text = license
+            }
         }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
