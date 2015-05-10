15f0bf832c5b22e5fbc97feeb9453dad366599d9
Nicholas Reichert <joe13reich@gmail.com>
2015-05-09 20:36:27 -0400

    Added ScheduleParser class with empty implementation.

---
+++ XScheduleKit/ScheduleParser.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleParser.swift
+//  X Schedule
+//
+//  Created by Nicholas Reichert on 5/9/15.
+//  Copyright (c) 2015 Nicholas Reichert.
+//
+
+import Foundation
+
+public class ScheduleParser {
+    
+    public func parseForSchedule(string: String, date: NSDate) -> Schedule {
+        return Schedule(items: [])
+    }
+    
+}
