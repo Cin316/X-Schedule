074f22755d7c075ca5a61670037720e38240ce95
Nicholas Reichert <joe13reich@gmail.com>
2015-05-27 15:50:41 -0400

    Added blank ScheduleManager and XScheduleManager classes.

---
+++ XScheduleKit/XScheduleManager.swift
@@ -0,0 +1,13 @@
+//
+//  XScheduleManager.swift
+//  X Schedule
+//
+//  Created by Nicholas Reichert on 5/27/15.
+//  Copyright (c) 2015 Nicholas Reichert.
+//
+
+import Foundation
+
+public class XScheduleManager: ScheduleManager {
+    
+}
