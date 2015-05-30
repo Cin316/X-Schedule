06186ad0584731245fdae8ee4d9dcc8c37884061
Nicholas Reichert <joe13reich@gmail.com>
2015-05-30 16:04:27 -0400

    Added implementation to XScheduleManager that supports caching.

--- XScheduleKit/XScheduleManager.swift
+++ XScheduleKit/XScheduleManager.swift
@@ -10,11 +10,28 @@ import Foundation
 
 public class XScheduleManager: ScheduleManager {
     public override class func getScheduleForDate(date: NSDate, completionHandler: Schedule -> Void, errorHandler: String -> Void) -> NSURLSessionTask? {
+        var task: NSURLSessionTask?
+        
+        if (CacheManager.scheduleExistsForDate(date)) {
+            getCachedScheduleForDate(date, completionHandler: completionHandler)
+        } else {
+            task = downloadScheduleForDate(date, completionHandler: completionHandler, errorHandler: errorHandler)
+        }
+        
+        return task
+    }
+    private class func getCachedScheduleForDate(date: NSDate, completionHandler: Schedule -> Void) {
+        var schedule: Schedule = CacheManager.loadScheduleForDate(date)
+        completionHandler(schedule)
+    }
+    private class func downloadScheduleForDate(date: NSDate, completionHandler: Schedule -> Void, errorHandler: String -> Void) -> NSURLSessionTask {
         var task: NSURLSessionTask
+        
         task = XScheduleDownloader.downloadSchedule(date, completionHandler: { (output: String) in
             var schedule: Schedule
             schedule = XScheduleParser.parseForSchedule(output, date: date)
             completionHandler(schedule)
+            CacheManager.cacheSchedule(schedule)
         }, errorHandler: errorHandler)
         
         return task
