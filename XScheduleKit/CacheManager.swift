9af4b9886095e91e25baed1176765ee423964028
Nicholas Reichert <joe13reich@gmail.com>
2015-05-30 15:35:22 -0400

    Created CacheManager class with blank methods.

---
+++ XScheduleKit/CacheManager.swift
@@ -0,0 +1,31 @@
+//
+//  CacheManager.swift
+//  X Schedule
+//
+//  Created by Nicholas Reichert on 5/30/15.
+//  Copyright (c) 2015 Nicholas Reichert.
+//
+
+import Foundation
+
+public class CacheManager {
+    
+    public static let defaultCacheLengthInDays = 30
+    
+    public class func scheduleExistsForDate(date: NSDate) -> Bool {
+    
+    }
+    public class func loadScheduleForDate(date: NSDate) -> Schedule {
+    
+    }
+    public class func cacheSchedule(schedule: Schedule) {
+    
+    }
+    
+    public class func buildCache() {
+        buildCacheForLengthOfTime(numOfDays: defaultCacheLengthInDays)
+    }
+    public class func buildCacheForLengthOfTime(numOfDays days: Int) {
+    
+    }
+}
