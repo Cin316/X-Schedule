f87bda36ce210fa94f38dcf172fbb2c56c73152f
Nicholas Reichert <joe13reich@gmail.com>
2015-06-10 09:28:31 -0400

    Added method for storing and loading substitutions data.

--- X Schedule/CustomizationViewController.swift
+++ X Schedule/CustomizationViewController.swift
@@ -11,6 +11,8 @@ import XScheduleKit
 
 class CustomizationViewController: UITableViewController {
     
+    static let substitutionsKey: String = "substitutions"
+    
     var substitutions: [(block: String, className: String)] = []
     
     override func viewDidLoad() {
@@ -38,5 +40,43 @@ class CustomizationViewController: UITableViewController {
         }
         
         return cell
+    }
+    
+    class func saveSubstitutions(subs: [(block: String, className: String)]) {
+        var arrayVersion: [[String]] = convertTupleToArray(subs)
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setObject(arrayVersion, forKey: substitutionsKey)
+    }
+    private class func convertTupleToArray(tuple: [(block: String, className: String)]) -> [[String]] {
+        var output: [[String]] = [[]]
+        for item in tuple {
+            var tmpArray = ["",""]
+            tmpArray[0] = item.block
+            tmpArray[1] = item.className
+            output.append(tmpArray)
+        }
+        
+        return output
+    }
+    private class func convertArrayToTuple(array: [[String]]) -> [(block: String, className: String)] {
+        var output: [(block: String, className: String)] = []
+        for item in array {
+            output.append(block: item[0], className: item[1])
+        }
+        
+        return output
+    }
+    class func loadSubstitutions() -> [(block: String, className: String)] {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        var object: AnyObject? = defaults.objectForKey(substitutionsKey)
+        
+        var tupleVersion: [(block: String, className: String)] = []
+        if let array = object as? [[String]] {
+            tupleVersion = convertArrayToTuple(array)
+            
+        }
+        
+        return tupleVersion
     }
 }
