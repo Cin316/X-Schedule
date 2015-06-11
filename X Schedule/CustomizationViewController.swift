153cfae2af1126be2b46da397d357c2182ef211a
Nicholas Reichert <joe13reich@gmail.com>
2015-06-11 08:56:20 -0400

    Made CustomizationViewController use new SubstitutionsManager. Linked CustomizationViewController with storyboard.

--- X Schedule/CustomizationViewController.swift
+++ X Schedule/CustomizationViewController.swift
@@ -11,12 +11,10 @@ import XScheduleKit
 
 class CustomizationViewController: UITableViewController {
     
-    static let substitutionsKey: String = "substitutions"
-    
-    var substitutions: [(block: String, className: String)] = []
+    var substitutions: [(block: String, className: String)] = SubstitutionManager.loadSubstitutions()
     
     override func viewDidLoad() {
-    
+        
     }
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -28,7 +26,7 @@ class CustomizationViewController: UITableViewController {
         return substitutions.count
     }
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("CustomizatioinCell", forIndexPath: indexPath) as! UITableViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier("CustomizationCell", forIndexPath: indexPath) as! UITableViewCell
         let item = substitutions[indexPath.row]
         
         // Configure the cell.
@@ -42,41 +40,4 @@ class CustomizationViewController: UITableViewController {
         return cell
     }
     
-    class func saveSubstitutions(subs: [(block: String, className: String)]) {
-        var arrayVersion: [[String]] = convertTupleToArray(subs)
-        
-        let defaults = NSUserDefaults.standardUserDefaults()
-        defaults.setObject(arrayVersion, forKey: substitutionsKey)
-    }
-    private class func convertTupleToArray(tuple: [(block: String, className: String)]) -> [[String]] {
-        var output: [[String]] = [[]]
-        for item in tuple {
-            var tmpArray = ["",""]
-            tmpArray[0] = item.block
-            tmpArray[1] = item.className
-            output.append(tmpArray)
-        }
-        
-        return output
-    }
-    private class func convertArrayToTuple(array: [[String]]) -> [(block: String, className: String)] {
-        var output: [(block: String, className: String)] = []
-        for item in array {
-            output.append(block: item[0], className: item[1])
-        }
-        
-        return output
-    }
-    class func loadSubstitutions() -> [(block: String, className: String)] {
-        let defaults = NSUserDefaults.standardUserDefaults()
-        var object: AnyObject? = defaults.objectForKey(substitutionsKey)
-        
-        var tupleVersion: [(block: String, className: String)] = []
-        if let array = object as? [[String]] {
-            tupleVersion = convertArrayToTuple(array)
-            
-        }
-        
-        return tupleVersion
-    }
 }
