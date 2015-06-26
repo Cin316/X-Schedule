3666dc315b8d4870534183a2856c7234eefa1002
Nicholas Reichert <joe13reich@gmail.com>
2015-06-26 08:50:11 -0400

    Added basic implementation to DataPageViewControllerDataSource.

--- X Schedule/DataPageViewController.swift
+++ X Schedule/DataPageViewController.swift
@@ -7,19 +7,43 @@
 //
 
 import UIKit
+import XScheduleKit
 
 class DataPageViewController: UIPageViewController {
-    
+    override func viewDidLoad() {
+        
+    }
 }
 
 class DataPageViewControllerDataSource: NSObject, UIPageViewControllerDataSource {
+    private static let scheduleIdentifier = "ColoredScheduleTableViewController"
     
     func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
-        return nil
+        var outputController: ScheduleTableController?
+        if let tableController = viewController as? ScheduleTableController {
+            var date: NSDate = tableController.schedule.date.dateByAddingTimeInterval(-24*60*60)
+            outputController = newScheduleTableForDate(date, pageController: pageViewController)
+        }
+        
+        return outputController
     }
     
     func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
-        return nil
+        var outputController: ScheduleTableController?
+        if let tableController = viewController as? ScheduleTableController {
+            var date: NSDate = tableController.schedule.date.dateByAddingTimeInterval(24*60*60)
+            outputController = newScheduleTableForDate(date, pageController: pageViewController)
+        }
+        
+        return outputController
+    }
+    
+    private func newScheduleTableForDate(date: NSDate, pageController: UIPageViewController) -> ScheduleTableController? {
+        var newTable: ScheduleTableController? = pageController.storyboard!.instantiateViewControllerWithIdentifier(DataPageViewControllerDataSource.scheduleIdentifier) as! ScheduleTableController?
+        //TODO Generate schedule.
+        newTable!.schedule = Schedule()
+        
+        return newTable
     }
     
 }
