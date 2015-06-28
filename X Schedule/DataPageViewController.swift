c1fc776604f2a8cba4341561806f783a38d43327
Nicholas Reichert <joe13reich@gmail.com>
2015-06-28 18:42:06 -0400

    Made schedule UI use page view now. Doesn't display schedule, but app doesn't crash.

--- X Schedule/DataPageViewController.swift
+++ X Schedule/DataPageViewController.swift
@@ -10,40 +10,52 @@ import UIKit
 import XScheduleKit
 
 class DataPageViewController: UIPageViewController {
+    var pageDataSource = DataPageViewControllerDataSource()
+    
     override func viewDidLoad() {
-        
+        self.dataSource = pageDataSource
+        var initialController = pageDataSource.pageChanged(nil, pageViewController: self, currentController: nil)!
+        self.setViewControllers([initialController], direction: UIPageViewControllerNavigationDirection.Forward, animated: false, completion: {(bool: Bool) in })
     }
 }
 
 class DataPageViewControllerDataSource: NSObject, UIPageViewControllerDataSource {
+    
     private static let scheduleIdentifier = "ColoredScheduleTableViewController"
     
+    enum Direction {
+        case Before
+        case After
+    }
+    
     func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
-        var outputController: ScheduleTableController?
-        if let tableController = viewController as? ScheduleTableController {
-            var date: NSDate = tableController.schedule.date.dateByAddingTimeInterval(-24*60*60)
-            outputController = newScheduleTableForDate(date, pageController: pageViewController)
-        }
-        
-        return outputController
+        return pageChanged(.Before, pageViewController: pageViewController, currentController: viewController)
     }
-    
     func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
-        var outputController: ScheduleTableController?
-        if let tableController = viewController as? ScheduleTableController {
-            var date: NSDate = tableController.schedule.date.dateByAddingTimeInterval(24*60*60)
-            outputController = newScheduleTableForDate(date, pageController: pageViewController)
-        }
-        
-        return outputController
+        return pageChanged(.After, pageViewController: pageViewController, currentController: viewController)
     }
     
-    private func newScheduleTableForDate(date: NSDate, pageController: UIPageViewController) -> ScheduleTableController? {
-        var newTable: ScheduleTableController? = pageController.storyboard!.instantiateViewControllerWithIdentifier(DataPageViewControllerDataSource.scheduleIdentifier) as! ScheduleTableController?
-        //TODO Generate schedule.
-        newTable!.schedule = Schedule()
+    private func pageChanged(direction: Direction?, pageViewController: UIPageViewController, currentController: UIViewController?) -> UIViewController? {
+        var date: NSDate = dateForViews(direction, currentController: currentController)
+        var outputController: ScheduleTableController? = pageViewController.storyboard!.instantiateViewControllerWithIdentifier(DataPageViewControllerDataSource.scheduleIdentifier) as! ScheduleTableController?
+        return outputController
+    }
+    private func dateForViews(direction: Direction?, currentController: UIViewController?) -> NSDate {
+        var date: NSDate
+        if let tableController = currentController as? ScheduleTableController {
+            var timeCoeff: Double
+            switch direction! {
+            case .Before:
+                timeCoeff = -1.0
+            case .After:
+                timeCoeff = 1.0
+            }
+            date = tableController.schedule.date.dateByAddingTimeInterval(timeCoeff*24*60*60)
+        } else {
+            date = NSDate()
+        }
         
-        return newTable
+        return date
     }
     
 }
