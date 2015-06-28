0820352553993cb415fd75834c3a58758d4f7d26
Nicholas Reichert <joe13reich@gmail.com>
2015-06-28 18:51:29 -0400

    Added blank SwipeDataViewController class and file.

---
+++ X Schedule/SwipeDataViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SwipeDataViewController.swift
+//  X Schedule
+//
+//  Created by Nicholas Reichert on 6/28/15.
+//  Copyright (c) 2015 Nicholas Reichert.
+//
+
+import Foundation
+
+class SwipeDataViewController: DataViewController {
+    
+}
