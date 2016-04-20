383460d4a09093546bea2860f6c8c9de832c1624
Nicholas Reichert <joe13reich@gmail.com>
2016-04-19 20:37:36 -0400

    Added template files for a basic watchOS app.

---
+++ X Schedule Watch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  X Schedule Watch Extension
+//
+//  Created by Nicholas Reichert on 4/19/16.
+//  Copyright © 2016 Nicholas Reichert. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
