d176f6f0aeb0e8b3d7c816893ac065e8635a7aec
Nicholas Reichert <joe13reich@gmail.com>
2017-05-27 11:49:33 -0400

    Added in detection if a schedule is unusual.

--- X Schedule/UnusualScheduleNotificationManager.swift
+++ X Schedule/UnusualScheduleNotificationManager.swift
@@ -11,18 +11,63 @@ import XScheduleKit
 
 class UnusualScheduleNotificationManager {
     
+    private static let usualSchedules: [String] = [
+        "",
+        "A Day",
+        "G Day",
+        "E Day",
+        "C Day",
+        "X Day",
+        "Y Day"
+    ]
+    
+    private static var scheduleTitles: [String]?
+    private static var scheduleTitlesLoaded = 0
+    class func getScheduleTitles() -> [String] {
+        // Use a singleton-like pattern for scheduleTitles.  scheduleTitles is nil until it's accessed, then create one shared instance.
+        if let titles = scheduleTitles {
+            return titles
+        } else {
+            // Initialize scheduleTitles to an empty array.
+            scheduleTitles = []
+            
+            // Load the past month and the next month of schedules.
+            let days = CacheManager.defaultCacheLengthInDays
+            for i in -days...days {
+                let date: Date = dateDaysFromNow(numOfDays: i)
+                XScheduleManager.getScheduleForDate(date, completionHandler: { (schedule: Schedule) in
+                    scheduleTitles?.append(schedule.title)
+                    scheduleTitlesLoaded += 1
+                }, errorHandler: { (errorText: String) in
+                    scheduleTitlesLoaded += 1
+                }, method: .cache)
+            }
+            
+            // Wait for schedules to finish loading.
+            while scheduleTitlesLoaded<=days*2 {
+                // The code inside this while loop does not usually get called, because loading from the cache is so fast.
+                usleep(10_000) // Sleep for 0.01 seconds (10,000 millionths of a second)
+            }
+            
+            return scheduleTitles!
+        }
+    }
+    private class func dateDaysFromNow(numOfDays days: Int) -> Date {
+        return Date().addingTimeInterval(Double(days*24*60*60))
+    }
+    
     // Note: I do not use UserNotifications methods because they are not backwards-compatible with older versions of iOS
     class func requestAuthorizationForNotifications() {
         UIApplication.shared.registerUserNotificationSettings(UIUserNotificationSettings(types: [.sound, .alert], categories: nil))
-        UIApplication.shared.setMinimumBackgroundFetchInterval(60*30) // Minimum fetch interval: 30 minutes.  This method must be called for background fetch to work
+        UIApplication.shared.setMinimumBackgroundFetchInterval(60*15/*60*30*/) // Minimum fetch interval: 30 minutes.  This method must be called for background fetch to work
     }
     
     // TODO Prevent sending repeated notifications.
     
     class func backgroundAppRefresh(_ completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        if (currentHour() < 14) { // 12AM-10AM: Look for today's schedule.
+        if (currentHour() < 10) { // 12AM-10AM: Look for today's schedule.
             lookForUnusualSchedule(onDate: Date(), dayWord: "Today", completionHandler: completionHandler)
-        } else if (14 <= currentHour() && currentHour() < 17) { // 10AM-5PM: Don't look for unusual schedules.
+        } else if (10 <= currentHour() && currentHour() < 17) { // 10AM-5PM: Don't look for unusual schedules.
             completionHandler(.noData)
         } else { // 17 <= currentHour // 5PM-12PM: Look for tomorrow's schedule.
             lookForUnusualSchedule(onDate: Date().addingTimeInterval(60*60*24), dayWord: "Tomorrow", completionHandler: completionHandler)
@@ -48,10 +93,17 @@ class UnusualScheduleNotificationManager {
         }, method: .download)
     }
     private class func isUnusual(schedule: Schedule) -> Bool {
-        return isRare(schedule: schedule) || hasManualTrigger(schedule: schedule)
+        return (!usualSchedules.contains(schedule.title) && isRare(schedule: schedule)) || hasManualTrigger(schedule: schedule)
     }
     private class func isRare(schedule: Schedule) -> Bool {
-        return true // TODO Implement rare detection
+        var occurances = 0
+        for title in getScheduleTitles() {
+            if title == schedule.title {
+                occurances += 1
+            }
+        }
+        
+        return occurances < 3
     }
     private class func hasManualTrigger(schedule: Schedule) -> Bool {
         return false // TODO Implement manual trigger support
